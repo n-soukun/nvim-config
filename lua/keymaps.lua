7003d8674906ed3e4c2f39c9298798dd3ae1a3c4
Soukun <73824766+n-soukun@users.noreply.github.com>
2023-07-30 23:33:28 +0900

    first commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,56 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+local keymap = vim.keymap.set
+
+keymap("n", "<M-n>", ":NeoTreeShowToggle<Return>", opts)
+
+-- buffer‚ÌˆÚ“®
+keymap("n", "<C-n>", ":bnext<Return>", opts)
+keymap("n", "<C-p>", ":bprevious<Return>", opts)
+
+-- split window
+keymap('n', 'ss', ':split<Return><C-w>w', opts)
+keymap('n', 'sv', ':vsplit<Return><C-w>w', opts)
+
+-- move buffer
+keymap('', 'sh', '<C-w>h')
+keymap('', 'sk', '<C-w>k')
+keymap('', 'sj', '<C-w>j')
+keymap('', 'sl', '<C-w>l')
+
+-- Move to previous/next
+keymap('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+keymap('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+keymap('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+keymap('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+keymap('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+keymap('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+keymap('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+keymap('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+keymap('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+keymap('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+keymap('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+keymap('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+keymap('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+keymap('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+keymap('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+keymap('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+keymap('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+keymap('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+keymap('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
---
+++ lua/plugins.lua
@@ -0,0 +1,58 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	--
+	-- この下にインストールするプラグインを記載します。
+	use("folke/tokyonight.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+	use("nvim-tree/nvim-web-devicons")
+    use('romgrk/barbar.nvim')
+    use("echasnovski/mini.indentscope")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use({
+		"folke/noice.nvim",
+		config = function()
+			require("noice").setup({
+				-- add any options here
+			})
+		end,
+		requires = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	})
+    use {
+     "nvim-neo-tree/neo-tree.nvim",
+     branch = "v2.x",
+     requires = {
+       "nvim-lua/plenary.nvim",
+       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+       "MunifTanjim/nui.nvim",
+     }
+    }
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+end)
